ac7566b6897cb8a263f4a0e70eb8c0e1507e8367
William <wosteimer1@gmail.com>
2024-05-10 13:16:19 -0300

    refactor(tests): Add description to tests

--- tests/test_base.py
+++ tests/test_base.py
@@ -10,6 +10,7 @@ from builder.base import (
 
 
 def test_case_1():
+    """It is expected that it will be possible to build the props"""
     result = build_props(
         {"john": "wick", "is_true": True, "is_false": False, "number": 2469}
     )
@@ -17,21 +18,26 @@ def test_case_1():
 
 
 def test_case_2():
+    """It is expected that it will be possible to build the classes"""
     result = build_classes([f"class_{n + 1}" for n in range(5)])
     assert result == ' class="class_1 class_2 class_3 class_4 class_5"'
 
 
 def test_case_3():
+    """It is expected that it will be possible to build the styles"""
     result = build_style({"margin": "auto", "padding": "16px"})
     assert result == ' style="margin:auto;padding:16px;"'
 
 
 def test_case_4():
+    """It is expected that if the child is a text, the result will be itself"""
     result = build_child("teste")
     assert result == "teste"
 
 
 def test_case_5():
+    """It is expected that if the child is a tag, the result will be an HTML tag"""
+
     class Test(ContentTag): ...
 
     result = build_child(Test())
@@ -39,6 +45,8 @@ def test_case_5():
 
 
 def test_case_6():
+    """It is expected that if the child is an empty tag, the result will be an empty HTML tag"""
+
     class Test(EmptyTag): ...
 
     result = build_child(Test())
@@ -46,62 +54,62 @@ def test_case_6():
 
 
 def test_case_7():
-    class Test(ContentTag): ...
+    """Classes are expected to render correctly in the HTML tag"""
 
-    result = build_child(Test(hello="world"))
-    assert result == '<test hello="world"></test>'
+    class TestContent(ContentTag): ...
+
+    class TestEmpty(EmptyTag): ...
+
+    result_content = TestContent(classes=[f"class_{n + 1}" for n in range(5)]).build()
+    result_empty = TestEmpty(classes=[f"class_{n + 1}" for n in range(5)]).build()
+    assert (
+        result_content
+        == '<test-content class="class_1 class_2 class_3 class_4 class_5"></test-content>'
+    )
+    assert (
+        result_empty == '<test-empty class="class_1 class_2 class_3 class_4 class_5"/>'
+    )
 
 
 def test_case_8():
-    class Test(EmptyTag): ...
+    """Styles are expected to be rendered correctly in the HTML tag"""
 
-    result = build_child(Test(hello="world"))
-    assert result == '<test hello="world"/>'
+    class TestContent(ContentTag): ...
+
+    class TestEmpty(EmptyTag): ...
+
+    result_content = TestContent(style={"margin": "auto", "padding": "16px"}).build()
+    result_empty = TestEmpty(style={"margin": "auto", "padding": "16px"}).build()
+    assert (
+        result_content
+        == '<test-content style="margin:auto;padding:16px;"></test-content>'
+    )
+    assert result_empty == '<test-empty style="margin:auto;padding:16px;"/>'
 
 
 def test_case_9():
-    class Test(ContentTag): ...
+    """Props are expected to render correctly in the HTML tag"""
 
-    result = Test(classes=[f"class_{n + 1}" for n in range(5)]).build()
-    assert result == '<test class="class_1 class_2 class_3 class_4 class_5"></test>'
+    class TestContent(ContentTag): ...
+
+    class TestEmpty(EmptyTag): ...
+
+    result_content = TestContent(
+        john="wick", is_true=True, is_false=False, number=2469
+    ).build()
+    result_empty = TestEmpty(
+        john="wick", is_true=True, is_false=False, number=2469
+    ).build()
+    assert (
+        result_content
+        == '<test-content john="wick" is-true number="2469"></test-content>'
+    )
+    assert result_empty == '<test-empty john="wick" is-true number="2469"/>'
 
 
 def test_case_10():
-    class Test(EmptyTag): ...
+    """Children are expected to be rendered correctly in the HTML tag"""
 
-    result = Test(classes=[f"class_{n + 1}" for n in range(5)]).build()
-    assert result == '<test class="class_1 class_2 class_3 class_4 class_5"/>'
-
-
-def test_case_11():
-    class Test(ContentTag): ...
-
-    result = Test(style={"margin": "auto", "padding": "16px"}).build()
-    assert result == '<test style="margin:auto;padding:16px;"></test>'
-
-
-def test_case_12():
-    class Test(EmptyTag): ...
-
-    result = Test(style={"margin": "auto", "padding": "16px"}).build()
-    assert result == '<test style="margin:auto;padding:16px;"/>'
-
-
-def test_case_13():
-    class Test(ContentTag): ...
-
-    result = Test(john="wick", is_true=True, is_false=False, number=2469).build()
-    assert result == '<test john="wick" is-true number="2469"></test>'
-
-
-def test_case_14():
-    class Test(EmptyTag): ...
-
-    result = Test(john="wick", is_true=True, is_false=False, number=2469).build()
-    assert result == '<test john="wick" is-true number="2469"/>'
-
-
-def test_case_15():
     class Test(ContentTag): ...
 
     class Test2(ContentTag): ...
@@ -110,20 +118,23 @@ def test_case_15():
     assert result == "<test><test2></test2></test>"
 
 
-def test_case_16():
+def test_case_11():
+    """It is expected that the name is constructed correctly"""
     result = build_name("TestNameIsCorrect")
     assert result == "test-name-is-correct"
 
 
-def test_case_18():
-    class TestNameIsCorrect(ContentTag): ...
+def test_case_12():
+    """The name is expected to be rendered correctly in the HTML tag"""
 
-    result = TestNameIsCorrect().build()
-    assert result == "<test-name-is-correct></test-name-is-correct>"
+    class TestNameIsCorrectContent(ContentTag): ...
 
+    class TestNameIsCorrectEmpty(EmptyTag): ...
 
-def test_case_19():
-    class TestNameIsCorrect(EmptyTag): ...
-
-    result = TestNameIsCorrect().build()
-    assert result == "<test-name-is-correct/>"
+    result_content = TestNameIsCorrectContent().build()
+    result_empty = TestNameIsCorrectEmpty().build()
+    assert (
+        result_content
+        == "<test-name-is-correct-content></test-name-is-correct-content>"
+    )
+    assert result_empty == "<test-name-is-correct-empty/>"
